734a15bf9dcfa48386c29d45f9311a75285fe090
Arthur Borges <123407087+Arthursenai@users.noreply.github.com>
2023-11-25 22:19:52 -0300

    Refactor CardsList to use new Cards class

--- src/models/cards/CardsList.js
+++ src/models/cards/CardsList.js
@@ -1,11 +1,12 @@
 import { cardsBody } from "../../data/cardsbody.js";
+import { Cards } from "../cards/cards.js";
 export class CardsList {
     constructor() {
         this.cards = [];
-        const cardBody = cardsBody;
-        this.cards.push(cardBody); 
+        const cards = cardsBody.map(card => new Cards(card.name, card.image, card.type, card.rarity, card.elixir, card.hp, card.deploytime, card.shieldhp, card.description, card.damage, card.damagepersecond, card.rangeddamage, card.damageondistance, card.damageonarea, card.damageonimpact, card.damageontower, card.chargedamage, card.damageondeath, card.spawnspeed, card.duration, card.radius, card.width, card.efecttime, card.freezetime, card.unities, card.arena, card.target, card.projectilerange, card.range, card.speed, card.impactspeed));
+        this.cards.push(cards);
+        console.log(this.cards);
     }
-
     addCard(card) {
         this.cards.push(card);
     }
