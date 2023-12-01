e61c47cf8c379614e90fdf427648c398559588f9
arthurborges <arthurborges.senai564@gmail.com>
2023-12-01 14:57:06 -0300

    Refactor CardsList constructor to remove unnecessary code

--- src/models/cards/CardsList.js
+++ src/models/cards/CardsList.js
@@ -1,17 +1,4 @@
 
-import { cardsBody } from "../../data/cardsbody.js";
-import { Cards } from "./Cards.js";
-export class CardsList {
-    constructor() {
-        this.cards = [];
-        const cards = cardsBody.map(card => new Cards(card.name, card.image, card.type, card.rarity, card.elixir, card.hp, card.deploytime, card.shieldhp, card.description, card.damage, card.damagepersecond, card.rangeddamage, card.damageondistance, card.damageonarea, card.damageonimpact, card.damageontower, card.chargedamage, card.damageondeath, card.spawnspeed, card.duration, card.radius, card.width, card.efecttime, card.freezetime, card.unities, card.arena, card.target, card.projectilerange, card.range, card.speed, card.impactspeed));
-        this.cards.push(...cards);
-        console.log(this.cards);
-    }
-    addCard(card) {
-        this.cards.push(card);
-    }
-
 export class CardsList {
   constructor() {
     this.cards = [];
