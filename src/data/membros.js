7fd52bafc787969de1ce0d2ef23ff6fa41358b25
matheuscosta <matheuszambon564@gmail.com>
2023-12-01 10:38:45 -0300

    Remove unnecessary id properties from integrantes array

--- src/data/membros.js
+++ src/data/membros.js
@@ -1,6 +1,5 @@
 export const integrantes = [
     {
-        id:1,
         nome:"Arthur",
         idade: 17,
         cargo:"Tech Lead",
@@ -8,7 +7,6 @@ export const integrantes = [
         descricao:"Arthur é um estudante dedicado do curso de Desenvolvimento de Sistemas no SENAI. Com sua paixão pela programação, ele se destaca por sua habilidade em resolver problemas complexos e sua capacidade de pensar de forma lógica."
     },
     {
-        id:2,
         nome:"Isabela",
         idade: 16,
         cargo:"Membro(a)",
@@ -16,7 +14,6 @@ export const integrantes = [
         descricao:"Isabela é uma integrante brilhante do curso, trazendo consigo uma abordagem criativa para o desenvolvimento de sistemas. Sua habilidade em pensar fora da caixa e encontrar soluções inovadoras faz dela uma presença valiosa em projetos de equipe."
     },
     {
-        id:3,
         nome:"João Victor",
         idade: 17,
         cargo:"Membro(a)",
@@ -24,7 +21,6 @@ export const integrantes = [
         descricao:"João é um entusiasta da tecnologia que está constantemente atualizado com as últimas tendências do setor. Sua curiosidade inata e paixão por aprender novas linguagens de programação o destacam como um estudante proativo."
     },
     {
-        id:4,
         nome:"Matheus Zambon", 
         idade: 17,
         cargo:"Membro(a)",
@@ -32,7 +28,6 @@ export const integrantes = [
         descricao:"Matheus é um membro do curso que se destaca por sua habilidade analítica e pensamento crítico. Sua abordagem metódica para resolver problemas torna-o um recurso valioso em situações desafiadoras."
     },
     {
-        id:5,
         nome:"Nicoly",
         idade: 16,
         cargo:"Membro(a)",
