eb7b64b0a17d606729374b02976f650767c6253c
isakamitech <isabelasouzade.564@gmail.com>
2023-12-14 17:27:12 -0300

    Update member images

--- src/data/membros.js
+++ src/data/membros.js
@@ -10,28 +10,28 @@ export const integrantes = [
         nome:"Isabela",
         idade: 16,
         cargo:"Membro(a)",
-        foto:"/images/isa.png",
+        foto:"/images/isa.jpg",
         descricao:"Isabela é uma integrante brilhante do curso, trazendo consigo uma abordagem criativa para o desenvolvimento de sistemas. Sua habilidade em pensar fora da caixa e encontrar soluções inovadoras faz dela uma presença valiosa em projetos de equipe."
     },
     {
         nome:"João Victor",
         idade: 17,
         cargo:"Membro(a)",
-        foto:"/images/jv.png",
+        foto:"/images/jv.jpg",
         descricao:"João é um entusiasta da tecnologia que está constantemente atualizado com as últimas tendências do setor. Sua curiosidade inata e paixão por aprender novas linguagens de programação o destacam como um estudante proativo."
     },
     {
         nome:"Matheus Zambon", 
         idade: 17,
         cargo:"Membro(a)",
-        foto:"/images/costa.png",
+        foto:"/images/costa.jpg",
         descricao:"Matheus é um membro do curso que se destaca por sua habilidade analítica e pensamento crítico. Sua abordagem metódica para resolver problemas torna-o um recurso valioso em situações desafiadoras."
     },
     {
         nome:"Nicoly",
         idade: 16,
         cargo:"Membro(a)",
-        foto:"/images/val.png",
+        foto:"/images/val.jpg",
         descricao:"Nicoly é reconhecida por sua habilidade de comunicação e trabalho em equipe exemplar. Sempre disposta a colaborar, ela desempenha um papel fundamental na coesão do grupo."
     }
 
